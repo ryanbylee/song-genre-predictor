29eb0ffb84a597312105b52fc198e4c5e1367278
Ryan Lee <ryanbylee@ucla.edu>
2024-10-11 10:39:15 -0700

    added exploratory data analysis

---
+++ dataloader.py
@@ -0,0 +1,24 @@
+import pandas as pd
+class DataLoader:
+    def __init__(self, split_percentage: list[int]):
+        self.df = pd.read_csv("hf://datasets/maharshipandya/spotify-tracks-dataset/dataset.csv")
+        # self.train, self.valid, self.test = self.split_data(split_percentage)
+
+    def print_data_info(self):
+        print(f"first five rows of data:\n{self.df.head(5)}")
+
+        # dimensions of the dataset
+        print(f"shape of dataset is: {self.df.shape}")
+
+        # variable types
+        print(f"variable types:\n{self.df.dtypes}")
+        
+    def split_data(self, split_percentage: list[int]):
+        # Split the data into train, validation and test sets
+        pass
+    
+    def preprocess_data(self):
+        # Preprocess the data
+        pass
+
+
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+from dataloader import DataLoader
+import matplotlib.pyplot as plt
+
 def main():
-    pass
+    dataset = DataLoader([0,0,0])
+    dataset.print_data_info()
+    print("\n------\n")
+
+    # plot the relationship between tempo and popularity
+    plt.scatter(dataset.df['tempo'], dataset.df['popularity'], s=0.1)
+    plt.xlabel('tempo')
+    plt.ylabel('popularity')
+    plt.title('tempo vs popularity')
+    plt.savefig('tempo_vs_popularity.png')
+
+    # plot the distirubtion of genre
+    dataset.df['track_genre'].value_counts().plot(kind='bar')
+    plt.xlabel('genre')
+    plt.ylabel('count')
+    plt.title('distribution of genre')
+    plt.savefig('genre_distribution.png')
+
+    # calcuate average popularity for each genre
+    avg_popularity = dataset.df.groupby('track_genre')['popularity'].mean()
+    # print top 5 with number of songs in each genre
+    # iterate through the top 5 genres above and print the number of songs in each genre
+    for genre in avg_popularity.sort_values(ascending=False).head(5).index:
+        print(f"Average popularity in genre {genre}: {avg_popularity[genre]}, Number of songs: {dataset.df[dataset.df['track_genre'] == genre].shape[0]}")
+
 
 if __name__ == '__main__':
     main()
